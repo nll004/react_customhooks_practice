acd28d638d9914ac68492e783a02b3a2e648705e
nll004 <loninath17@gmail.com>
2022-10-16 13:46:22 -0400

    useAxios working with playing cards

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import axios from "axios";
+import { v4 as uuid } from "uuid";
+
+
+function useAxios(initialState=[]){
+    const [cards, setCards] = useState(initialState);
+
+    async function addCard(url, name){
+        let res;
+        if(name) res = await axios.get(`${url}/${name}/`);
+        else     res = await axios.get(url);
+        setCards(cards => [...cards, { ...res.data, id: uuid() }]);
+    }
+
+    return [cards, addCard]
+}
+
+export default useAxios;
